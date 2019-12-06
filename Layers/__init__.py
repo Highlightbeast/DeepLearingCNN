e67440ed60b8019059e315983aad0227ee96f882
Highlightbeast <yuzhu.pang@fau.de>
2019-12-06 16:53:20 +0100

    Layer name

--- Layers/__init__.py
+++ Layers/__init__.py
@@ -1,2 +1,2 @@
 # __all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Conv", "Pooling", "Initializers", "Flatten"]
-__all__ = ["Helpers", "FullyConnected", "Initializers", "Flatten"]
+__all__ = ["Helpers", "FullyConnected", "Initializers", "Flatten",  "Conv", "SoftMax", "ReLU"]
